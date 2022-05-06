906c35a240ff12f6d8b1ee3c170ec4de249c3d40
balintgerinm <balintgerinm@gmail.com>
2022-05-06 09:04:53 +0200

    mockito and first tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,52 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
-
+  private TorpedoStore primary;
+  private TorpedoStore secondary;
+  
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    primary = mock(TorpedoStore.class);
+    secondary = mock(TorpedoStore.class);
+    this.ship = new GT4500(primary, secondary);
+
+    //when(primary.getTorpedoCount()).thenReturn(10);
+    //when(secondary.getTorpedoCount()).thenReturn(20);
+    
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primary.fire(1)).thenReturn(true);
+    when(primary.isEmpty()).thenReturn(false);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(primary, times(1)).fire(1);
+    verify(primary, times(1)).isEmpty();
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(primary.fire(1)).thenReturn(true);
+    when(primary.isEmpty()).thenReturn(false);
+    when(secondary.fire(1)).thenReturn(true);
+    when(secondary.isEmpty()).thenReturn(false);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(primary, times(1)).fire(1);
+    verify(primary, times(1)).isEmpty();
+    verify(secondary, times(1)).fire(1);
+    verify(secondary, times(1)).isEmpty();
   }
 
 }
